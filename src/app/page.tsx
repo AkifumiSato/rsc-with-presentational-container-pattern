7a4953f01a58cff09fd77720ec8dece5e43c1098
佐藤昭文 <akfm.sato@gmail.com>
2023-08-05 22:28:55 +0900

    RSC display query

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
 import { Search } from "@/app/_components/Search";
 import React from "react";
 
-export default function Home() {
+export default function Home({
+  searchParams,
+}: {
+  searchParams: {
+    q?: string;
+  };
+}) {
   return (
     <div className="flex justify-center">
       <article className="format lg:format-lg max-w-5xl min-w-full p-5">
         <h1>Search with RSC.</h1>
+        <p>Server rendered: `q`: {searchParams.q ?? "undefined"}</p>
         <Search />
       </article>
     </div>
